c958fd36c96cb5032d3c729815da7ced9e8a3eeb
Yusuke Ando <yandod@gmail.com>
2013-06-11 03:12:04 +0900

    reformat

--- cookbooks/candycane_cookbook/recipes/default.rb
+++ cookbooks/candycane_cookbook/recipes/default.rb
@@ -2,7 +2,7 @@ execute "apt-get" do
   command "apt-get update"
 end
 
-%{nginx php5 php5-mysql php5-cli php5-fpm php-pear mysql-server}.each do |pkg|
+%w{nginx php5 php5-mysql php5-cli php5-fpm php-pear mysql-server}.each do |pkg|
   package pkg do
     action [:install, :upgrade]
   end
@@ -18,14 +18,8 @@ template "/etc/nginx/conf.d/php-fpm.conf" do
   source "php-fpm.conf.erb"
 end
 
-service "mysqld" do
-  action [:start]
-end
-
-service "php5-fpm" do
-  action [:start, :restart]
-end
-
-service "nginx" do
-  action [:start, :restart]
+%w{mysql php5-fpm nginx}.each do |service_name|
+    service service_name do
+      action [:start, :restart]
+    end
 end
